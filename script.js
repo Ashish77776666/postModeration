1eb8d6b146bff2267cfd97e343687000f5bdb1b1
Ashish77776666 <76150664+Ashish77776666@users.noreply.github.com>
2024-04-10 20:30:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const photos = [
+    {
+      url: "Ashish_Kumar.jpg",
+      caption: "Beautiful sunset",
+      tags: ["nature", "sky"]
+    },
+    // ... Add more photo objects with URL, caption, and tags (up to 100)
+  ];
